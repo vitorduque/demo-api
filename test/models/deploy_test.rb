f5078684c943ac04c6c43556cbf3b65a62c87b9f
Vitor Duque <vduque@Vitors-MacBook-Pro.local>
2017-11-30 12:02:42 -0200

    Added tests for model attr

--- test/models/deploy_test.rb
+++ test/models/deploy_test.rb
@@ -1,8 +1,25 @@
 require 'test_helper'
 
 class DeployTest < ActiveSupport::TestCase
+
   test "should now be valid without feature" do
-    deploy = Deploy.new(feature: 'feature one', version
+    deploy = Deploy.new(version: '0.0.1', responsible: 'Someone', status: 'Docker build')
     assert_not deploy.valid?
   end
+
+  test "should now be valid without version" do
+    deploy = Deploy.new(feature: 'Some CRUD',  responsible: 'Someone', status: 'Docker build')
+    assert_not deploy.valid?
+  end
+
+  test "should now be valid without responsible" do
+    deploy = Deploy.new(feature: 'Some CRUD', version: '0.0.1',  status: 'Docker build')
+    assert_not deploy.valid?
+  end
+
+  test "should now be valid without status" do
+    deploy = Deploy.new(feature: 'Some CRUD', version: '0.0.1', status: 'Docker build')
+    assert_not deploy.valid?
+  end
+
 end
